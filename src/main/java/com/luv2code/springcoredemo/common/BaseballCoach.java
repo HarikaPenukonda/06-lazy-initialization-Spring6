5caaf6774095b189a4c04d799a2890f3c3dbd220
harika penukonda <penukondaharika@gmail.com>
2023-05-23 15:02:40 -0500

    Adding @Qualifier Annotation

--- src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
@@ -2,7 +2,6 @@ package com.luv2code.springcoredemo.common;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
-@Primary
 @Component
 public class BaseballCoach implements Coach{
     @Override
--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -13,7 +13,7 @@ public class DemoController {
 
     // define a private field for dependency
     @Autowired // Tell Spring to inject a dependency, optional if only one constructor
-    public DemoController(Coach myCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach myCoach) {
         this.myCoach = myCoach;
     }
 
